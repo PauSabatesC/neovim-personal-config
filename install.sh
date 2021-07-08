92c9e4295ef3ede2babcd3447d9a701eaf4f8489
Pau Sabatés Campos <camposabates@gmail.com>
2021-07-08 19:49:51 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -2,18 +2,27 @@
 echo 'Installing neovim'
 sudo add-apt-repository -y ppa:neovim-ppa/stable 
 sudo apt-get update
+sudo apt-get upgrade
 sudo apt-get install -y neovim
 
 echo 'Copying personal neovim config to ~/.config/nvim'
-mkdir ~/.config/nvim
-#touch ~/.config/nvim/init.vim
-cp init.vim ~/.config/nvim/
+mkdir -p ~/.config/nvim
+cp init.vim ~/.config/nvim/init.vim
+cp coc-settings.json ~/.config/nvim/coc-settings.json
 
 echo 'Installing Plug manager'
 sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
        https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
 
+echo 'Installing coc nvim nodejs'
+curl -sL https://deb.nodesource.com/setup_14.x -o nodesource_setup.sh
+bash nodesource_setup.sh
+apt install -y nodejs
+
 echo 'Installing Terraform language server'
+DEBIAN_FRONTEND=noninteractive
+sudo apt install -y lsb-core
+sudo apt install -y software-properties-common
 curl -fsSL https://apt.releases.hashicorp.com/gpg | sudo apt-key add -
 sudo apt-add-repository "deb [arch=amd64] https://apt.releases.hashicorp.com $(lsb_release -cs) main"
 sudo apt-get update && sudo apt-get install -y terraform-ls
