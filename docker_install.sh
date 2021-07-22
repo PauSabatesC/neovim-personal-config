95002fe6db9476a62517a3578d868a6c20419a8d
Pau Sabatés <camposabates@gmail.com>
2021-07-22 18:38:10 +0200

    Update docker_install.sh

--- docker_install.sh
+++ docker_install.sh
@@ -34,3 +34,4 @@ apt install -y ripgrep
 echo 'alias vim='nvim'' >> ~/.bashrc
 
 echo 'Now run :PlugInstall to install plugins and done!'
+echo 'Now run :CocInstall coc-tsserver to install typescript language server!'
